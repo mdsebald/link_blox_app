12724cb5f04d0326466653a6e076e6a92e34bebc
Mark Sebald <Mark Sebald>
2017-04-04 05:25:11 -0500

    Change "type_" to "lblx_"

--- src/demo_config.erl
+++ src/demo_config.erl
@@ -20,12 +20,12 @@
 % Load this demo configuration, in case default LinkBloxConfig file is not available.
 create_demo_config() ->
 
-  GreenLed = type_rpi_led:create(green_led, "On-Board Green LED",
+  GreenLed = lblx_rpi_led:create(green_led, "On-Board Green LED",
                                  [],  % Default Config values are good 
                                  [{disable, {false, ?EMPTY_LINK}},
                                   {input, {empty, {toggle, value}}}]
                                 ),
-  Toggle = type_toggle:create(toggle, "Toggle LED on / off",
+  Toggle = lblx_toggle:create(toggle, "Toggle LED on / off",
                               [],  % Default Config values are good 
                               [{disable, {false, ?EMPTY_LINK}},
                                {exec_interval, {250, ?EMPTY_LINK}}]
@@ -35,7 +35,7 @@ create_demo_config() ->
 
  % Don't use this demo_config, relies too much on I2C harware.
  create_demo_config1() ->
-    Test7Seg = type_int_to_7seg:create(test_7seg, "Testing Seven Segment Decoder",
+    Test7Seg = lblx_int_to_7seg:create(test_7seg, "Testing Seven Segment Decoder",
                                  [
                                      {num_of_digits, {4}}
                                  ],
@@ -45,20 +45,20 @@ create_demo_config() ->
                                      {input, {empty, {counter, value}}}
                                  ]),
     
-    RotEncoder = type_rotary_encoder:create(rotary_encode, "Rotary Encoder with Switch",
+    RotEncoder = lblx_rotary_encoder:create(rotary_encode, "Rotary Encoder with Switch",
                                 [],
                                  [
                                      {disable, {false, ?EMPTY_LINK}}
                                  ]),
                                                         
-    RmTemp = type_mcp9808_temp:create(room_temp, "Room Temp Sensor",
+    RmTemp = lblx_mcp9808_temp:create(room_temp, "Room Temp Sensor",
                                  [],
                                  [
                                      {disable, {false, ?EMPTY_LINK}},
                                      {exec_interval, {1000, ?EMPTY_LINK}}
                                  ]),
                                  
-    PbSwitch = type_gpio_di:create(switch_27, "Reset Counter",
+    PbSwitch = lblx_gpio_di:create(switch_27, "Reset Counter",
                                  [
                                      {gpio_pin, {27}}
                                  ], 
@@ -66,56 +66,56 @@ create_demo_config() ->
                                      {disable, {false, ?EMPTY_LINK}}
                                  ]),
 
-    Display = type_one_digit_7seg:create(display, "Decode count to seven-segments",
+    Display = lblx_one_digit_7seg:create(display, "Decode count to seven-segments",
                                  [],
                                  [
                                      {disable, {false, ?EMPTY_LINK}},
                                      {segments, {empty, {rotary_encode, value}}}
                                  ]),
                                  
-    SegA = type_gpio_do:create(seg_a, "LED Segment A",
+    SegA = lblx_gpio_do:create(seg_a, "LED Segment A",
                                  [{gpio_pin, {23}}, {invert_output, {false}}], 
                                  [{disable, {false, ?EMPTY_LINK}},
                                   {input, {empty, {display, seg_a}}}]
                                 ),
 
-    SegB = type_gpio_do:create(seg_b, "LED Segment B",
+    SegB = lblx_gpio_do:create(seg_b, "LED Segment B",
                                  [{gpio_pin, {24}}, {invert_output, {false}}], 
                                  [{disable, {false, ?EMPTY_LINK}},
                                   {input, {empty, {display, seg_b}}}]
                                 ),
 
-    SegC = type_gpio_do:create(seg_c, "LED Segment C",
+    SegC = lblx_gpio_do:create(seg_c, "LED Segment C",
                                  [{gpio_pin, {25}}, {invert_output, {false}}], 
                                  [{disable, {false, ?EMPTY_LINK}},
                                   {input, {empty, {display, seg_c}}}]
                                 ),
 
-    SegD = type_gpio_do:create(seg_d, "LED Segment D",
+    SegD = lblx_gpio_do:create(seg_d, "LED Segment D",
                                  [{gpio_pin, {26}}, {invert_output, {false}}], 
                                  [{disable, {false, ?EMPTY_LINK}},
                                   {input, {empty, {display, seg_d}}}]
                                 ),
 
-    SegE = type_gpio_do:create(seg_e, "LED Segment E",
+    SegE = lblx_gpio_do:create(seg_e, "LED Segment E",
                                  [{gpio_pin, {16}}, {invert_output, {false}}], 
                                  [{disable, {false, ?EMPTY_LINK}},
                                   {input, {empty, {display, seg_e}}}]
                                 ),
         
-    SegF = type_gpio_do:create(seg_f, "LED Segment F",
+    SegF = lblx_gpio_do:create(seg_f, "LED Segment F",
                                  [{gpio_pin, {22}}, {invert_output, {false}}], 
                                  [{disable, {false, ?EMPTY_LINK}},
                                   {input, {empty, {display, seg_f}}}]
                                 ),
         
-    SegG = type_gpio_do:create(seg_g, "LED Segment G",
+    SegG = lblx_gpio_do:create(seg_g, "LED Segment G",
                                  [{gpio_pin, {17}}, {invert_output, {false}}], 
                                  [{disable, {false, ?EMPTY_LINK}},
                                   {input, {empty, {display, seg_g}}}]
                                 ),
 
-    Counter = type_exec_count:create(counter, "Count to 3 and rollover",
+    Counter = lblx_exec_count:create(counter, "Count to 3 and rollover",
                                  [
                                     
                                  ],
@@ -127,7 +127,7 @@ create_demo_config() ->
                                      {final_value, {3, ?EMPTY_LINK}}
                                  ]),
 
-    EnvSensor = type_bme280:create(env_sensor, "Temp, Press, Humid Sensor",
+    EnvSensor = lblx_bme280:create(env_sensor, "Temp, Press, Humid Sensor",
                                  [
 
                                  ], 
@@ -136,7 +136,7 @@ create_demo_config() ->
                                     {exec_interval, {1000, ?EMPTY_LINK}}
                                 ]),
 
-   SelectVal = type_n_select:create(select_sensor, "Select Temp, Press, or Humid Sensor",
+   SelectVal = lblx_n_select:create(select_sensor, "Select Temp, Press, or Humid Sensor",
                                 [
 
                                 ],
@@ -146,7 +146,7 @@ create_demo_config() ->
                                   {inputs, [{empty, {env_sensor, temp}}, {empty, {env_sensor, press}}, {empty, {env_sensor, humid}}]}
                                 ]),
 
-    Decoder = type_float_to_7seg:create(decoder, "Decode Number to 7 Segment Digits",
+    Decoder = lblx_float_to_7seg:create(decoder, "Decode Number to 7 Segment Digits",
                                 [
 
                                 ], 
@@ -155,7 +155,7 @@ create_demo_config() ->
                                   {input, {empty, {select_sensor, value}}}
                                 ]),
 
-    LedDisp = type_ht16k33_4digit_led:create(led_disp, "4 Digit LED Display",
+    LedDisp = lblx_ht16k33_4digit_led:create(led_disp, "4 Digit LED Display",
                                  [
 
                                  ],
