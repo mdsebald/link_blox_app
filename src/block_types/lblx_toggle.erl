766c31bc65d0fe9e53f1af43a3f9bcf84a6cd331
mdsebald <mdsebald@gmail.com>
2017-05-15 06:02:02 -0500

    Add this to logic block type groups

--- src/block_types/lblx_toggle.erl
+++ src/block_types/lblx_toggle.erl
@@ -16,7 +16,7 @@
 -export([groups/0, description/0, version/0]).
 -export([create/2, create/4, create/5, upgrade/1, initialize/1, execute/2, delete/1]).
 
-groups() -> [none].
+groups() -> [logic].
 
 description() -> "Toggle binary output value on block execution".
 
